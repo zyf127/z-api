c722c63d1f62d38e13c4c9a8bcba28171bf9334e
zyf <zyf19863@163.com>
2024-04-30 23:29:17 +0800

    update 为用户表添加 access_key 字段和 secret_key 字段

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -14,6 +14,8 @@ create table if not exists user
     gender       tinyint                                null comment '性别',
     user_role     varchar(256) default 'user'            not null comment '用户角色：user / admin',
     user_password varchar(512)                           not null comment '密码',
+    access_key varchar(512)                           not null comment 'access_key',
+    secret_key varchar(512)                           not null comment 'secret_key',
     create_time   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     update_time   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     is_delete     tinyint      default 0                 not null comment '是否删除',
@@ -21,19 +23,19 @@ create table if not exists user
         unique (user_account)
 ) comment '用户';
 
--- 接口信息
-create table if not exists z_api.`interface_info`
+-- 接口信息表
+create table if not exists interface_info
 (
-    `id` bigint not null auto_increment comment '主键' primary key,
-    `name` varchar(256) not null comment '名称',
-    `description` varchar(256) null comment '描述',
-    `url` varchar(512) not null comment '接口地址',
-    `request_header` text null comment '请求头',
-    `response_header` text null comment '响应头',
-    `status` int default 0 not null comment '接口状态（0-关闭，1-开启）',
-    `method` varchar(256) not null comment '请求类型',
-    `user_id` bigint not null comment '创建人',
-    `create_time` datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    `update_time` datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    `is_delete` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
+    id bigint not null auto_increment comment '主键' primary key,
+    name varchar(256) not null comment '名称',
+    description varchar(256) null comment '描述',
+    url varchar(512) not null comment '接口地址',
+    request_header text null comment '请求头',
+    response_header text null comment '响应头',
+    status int default 0 not null comment '接口状态（0-关闭，1-开启）',
+    method varchar(256) not null comment '请求类型',
+    user_id bigint not null comment '创建人',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
 ) comment '接口信息';
